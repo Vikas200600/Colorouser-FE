53d3f39893420b4e94d8e954ef159c1209802f7e
Vikas M <vikas.m@zysk.in>
2021-12-20 18:12:59 +0530

    group component complete

--- src/app/modules/groups/groups.module.ts
+++ src/app/modules/groups/groups.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatCardModule } from '@angular/material/card';
+
 import { GroupsRoutingModule } from './groups-routing.module';
 import { GroupsComponent } from './groups.component';
 
-
 @NgModule({
-  declarations: [
-    GroupsComponent
-  ],
-  imports: [
-    CommonModule,
-    GroupsRoutingModule
-  ]
+  declarations: [GroupsComponent],
+  imports: [CommonModule, GroupsRoutingModule, MatCardModule],
 })
-export class GroupsModule { }
+export class GroupsModule {}
